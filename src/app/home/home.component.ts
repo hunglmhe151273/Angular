4f2563b99351fd685ade6a768c536f5df3c10288
hunglm2 <hunglm2@users.noreply.github.com>
2023-11-16 14:08:07 +0700

    Databinding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss' 
 })
 export class HomeComponent {
-
+  public name = 'Huy Nguyễn';
+ public age = 20;
+  constructor(){}
+  public RefreshName(): void {
+    console.log('refreshName');
+    this.name = '';
+  }
 }
